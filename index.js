3ee9a4b5f5c8a1e54ecf94aaf4b053af9a12bb5f
Karl Antle <kantle@gmail.com>
2018-02-06 10:49:49 -0500

    Changed to prop-types reference

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import {NetInfo, View, StatusBar, Animated, Easing, AppState} from 'react-native';
 import styles from './index.styles';
 
